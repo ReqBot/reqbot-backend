5e55658a6534f588f4c6cda060ccd2bebae12f1b
Andrés López <alopez@booz.cl>
2021-08-23 02:12:37 -0500

    Setup IBM Watson

---
+++ connection/connection.js
@@ -0,0 +1,13 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  //database: "",
+  user: "root",
+  password: "root"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});

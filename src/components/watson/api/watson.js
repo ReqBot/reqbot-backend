c2010061af532cc91dee6590f61079316edbb1cc
Andrés López <alopez@booz.cl>
2021-08-29 14:51:24 -0500

    IBM Watson Translate

--- src/components/watson/api/watson.js
+++ src/components/watson/api/watson.js
@@ -2,7 +2,14 @@
 const express = require("express");
 const router = express.Router();
 const AssistantV2 = require("ibm-watson/assistant/v2");
-const { IamAuthenticator } = require("ibm-watson/auth");
+const {
+  IamAuthenticator
+} = require("ibm-watson/auth");
+const fetch = require('node-fetch');
+const {
+  default: axios
+} = require('axios');
+//const axios = require('axios').default;
 
 // 2. Create Instance of Assistant
 
@@ -38,25 +45,47 @@ router.get("/session", async (req, res) => {
 // 4. Handle Messages
 // POST /api/watson/message
 router.post("/message", async (req, res) => {
-  // Construct payload
-  payload = {
-    assistantId: process.env.WATSON_ASSISTANT_ID,
-    sessionId: req.headers.session_id,
-    input: {
-      message_type: "text",
-      text: req.body.input,
-    },
-  };
-
-  // If successs
   try {
-    const message = await assistant.message(payload);
-    res.json(message["result"]);
+    const initialData = {
+      q: req.body.q,
+      source: "es",
+      target: "en"
+    };
 
-    // If fail
+    const axiosres = await axios.post("https://libretranslate.de/translate", initialData)
+    const translatedESEN = axiosres.data?.translatedText ?? '';
+
+    payload = {
+      assistantId: process.env.WATSON_ASSISTANT_ID,
+      sessionId: req.headers.session_id,
+      input: {
+        message_type: "text",
+        text: translatedESEN,
+      },
+    };
+
+    // If successs
+    const ibmRes = await assistant.message(payload);
+    const ibmMessage = ibmRes["result"].output.generic[0].text;
+
+    // Translate for user
+    const finalRes = await axios.post("https://libretranslate.de/translate", {
+      q: ibmMessage,
+      source: "en",
+      target: "es"
+    });
+
+    res.status(200).send({
+      success: true,
+      message: finalRes.data.translatedText ?? ''
+    });
+
   } catch (err) {
-    res.send("There was an error processing your request.");
     console.log(err);
+    res.status(500).send({
+      success: false,
+      message: "There was an error processing your request."
+    });
   }
 });
 

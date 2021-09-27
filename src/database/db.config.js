55d0678ee21574bf1507b497177cf9b9288fa78d
Andrés López <alopez@booz.cl>
2021-09-26 22:05:04 -0500

    Change DB

--- src/database/db.config.js
+++ src/database/db.config.js
@@ -1,16 +1,15 @@
 var mysql = require('mysql');
 
 var dbConn = mysql.createConnection({
-  host: "localhost",
-  database: "bdreqbot",
-  user: "root",
-  password: "root"
+  host: "us-cdbr-east-04.cleardb.com",
+  database: "heroku_5b6792719729bc7",
+  user: "b184d7b02f4177",
+  password: "fdb5cb53"
 });
 
 dbConn.connect(function(err) {
   if (err) throw err;
   console.log("Database connected!");
 });
-
 
 module.exports = dbConn;

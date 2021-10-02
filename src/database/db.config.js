37f1b7cba303a98b48a4c903d95748bdab1694f4
Andrés López <alopez@booz.cl>
2021-10-02 00:40:02 -0500

    Fix Tickets

--- src/database/db.config.js
+++ src/database/db.config.js
@@ -1,20 +1,20 @@
 var mysql = require('mysql');
 
-/*var dbConn = mysql.createPool({
+var dbConn = mysql.createPool({
   connectionLimit : 100, 
   host: "us-cdbr-east-04.cleardb.com",
   database: "heroku_5b6792719729bc7",
   user: "b184d7b02f4177",
   password: "fdb5cb53"
-});*/
-
+});
+/*
 var dbConn = mysql.createPool({
   connectionLimit : 100, 
   host: "localhost",
   database: "bdreqbot",
   user: "root",
   password: "root"
-})
+})*/
 
 /*
 var dbConn = mysql.createConnection({

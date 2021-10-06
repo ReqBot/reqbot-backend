5edb0bcf402c3a6965496b861b568d8b62226b6f
Andrés López <alopez@booz.cl>
2021-10-06 18:37:10 -0500

    Add Id in Get All Users by Organizations

--- src/components/usuario/model/usuario.model.js
+++ src/components/usuario/model/usuario.model.js
@@ -76,7 +76,7 @@ Usuario.findAll = function (result) {
 };
 
 Usuario.findByOrganizacion = function (id,result) {
-    dbConn.query('SELECT nombre,apellido,correo,rol,estado,idOrganizacion FROM usuario where idOrganizacion=?',id, function (err, res) {
+    dbConn.query('SELECT idUsuario,nombre,apellido,correo,rol,estado,idOrganizacion FROM usuario where idOrganizacion=?',id, function (err, res) {
         if (err) {
             console.log("error: ", err);
             result(null, err);
